ff4c756c9d2edf45f33495b5360c1a3f18b9eaa6
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-06-06 01:09:31 +0900

    design: 시작화면 디자인 반영

--- app/src/main/java/com/example/ratemate/login/StartScreen.kt
+++ app/src/main/java/com/example/ratemate/login/StartScreen.kt
@@ -3,11 +3,13 @@ package com.example.ratemate.login
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -15,47 +17,67 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.ratemate.R
-import com.example.ratemate.ui.theme.Purple40
+import com.example.ratemate.ui.theme.NotoSansKr
 
 @Composable
-fun StartScreen(navController: NavHostController){
+fun StartScreen(navController: NavHostController) {
 
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Bottom,
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
+    ) {
+        // PNG 이미지를 로드합니다.
+        val startLogo: Painter = painterResource(id = R.drawable.start_logo)
+        val startLogoTxt: Painter = painterResource(id = R.drawable.start_logo_txt)
 
-        val image : Painter = painterResource(id = R.drawable.logo)
         Image(
-            painter = image,
+            painter = startLogo,
             contentDescription = "Logo",
             modifier = Modifier
-                .size(200.dp)
-                .clip(RoundedCornerShape(50.dp))
-
+                .size(100.dp)
         )
 
+        Image(
+            painter = startLogoTxt,
+            contentDescription = "logo text",
+            modifier = Modifier
+                .padding(top = 28.dp)
+                .size(100.dp)
+        )
 
-        Spacer(modifier = Modifier.height(20.dp))
+        Spacer(modifier = Modifier.height(140.dp))
+
         Button(
             onClick = {
                 navController.navigate("login?mail=&pw=")
             },
             shape = MaterialTheme.shapes.large,
-            colors = ButtonDefaults.buttonColors(Purple40),
+            colors = ButtonDefaults.buttonColors(colorResource(id = R.color.main_blue)),
             modifier = Modifier
-                .size(100.dp, 40.dp)
-        ){
-            Text(text = "Login")
+                .padding(horizontal = 20.dp)
+                .fillMaxWidth()
+                .height(48.dp),
+            contentPadding = PaddingValues(vertical = 0.dp)
+        ) {
+            Text(
+                text = "로그인",
+                fontFamily = NotoSansKr,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(id = R.color.white)
+            )
         }
-
 
         Spacer(modifier = Modifier.height(16.dp))
         Button(
@@ -63,11 +85,28 @@ fun StartScreen(navController: NavHostController){
                 navController.navigate("register")
             },
             shape = MaterialTheme.shapes.large,
-            colors = ButtonDefaults.buttonColors(Purple40),
+            colors = ButtonDefaults.buttonColors(colorResource(id = R.color.main_blue)),
             modifier = Modifier
-                .size(100.dp, 40.dp)
-        ){
-            Text(text = "Register")
+                .padding(horizontal = 20.dp)
+                .fillMaxWidth()
+                .height(48.dp),
+            contentPadding = PaddingValues(vertical = 0.dp)
+        ) {
+            Text(
+                text = "회원가입",
+                fontFamily = NotoSansKr,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(id = R.color.white)
+            )
         }
+
+        Spacer(modifier = Modifier.height(100.dp))
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewStartScreen() {
+    StartScreen(navController = rememberNavController())
 }
